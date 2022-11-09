d9dd127ed8a6bd255cd7ed4f7fc0704b4307448e
gabrieldemian <gabrielgcr45@gmail.com>
2022-11-08 22:19:06 -0300

    static keys

--- src/models/p2p.rs
+++ src/models/p2p.rs
@@ -1,12 +1,17 @@
 // use super::blockchain::Blockchain;
 
 use libp2p::{
-    core::transport::MemoryTransport,
+    core::{transport::MemoryTransport, upgrade},
     gossipsub::{Gossipsub, GossipsubConfig, IdentTopic, MessageAuthenticity},
-    identity, mplex, multiaddr,
+    identity::Keypair,
+    mplex, multiaddr,
     noise::NoiseAuthenticated,
-    Multiaddr, PeerId, Swarm, Transport,
+    PeerId, Swarm, Transport,
 };
+use once_cell::sync::Lazy;
+
+static LOCAL_KEY: Lazy<Keypair> = Lazy::new(|| Keypair::generate_ed25519());
+static LOCAL_PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(LOCAL_KEY.public()));
 
 // defines the behaviour of the current peer
 // on the network
@@ -16,19 +21,16 @@ pub struct P2P {
 
 impl P2P {
     pub fn new() -> Self {
-        let local_key = identity::Keypair::generate_ed25519();
-        let local_peer_id = PeerId::from(local_key.public());
-
         // encrypted TCP transport over mplex
         let transport = MemoryTransport::default()
-            .upgrade(libp2p::core::upgrade::Version::V1)
-            .authenticate(NoiseAuthenticated::xx(&local_key).unwrap())
+            .upgrade(upgrade::Version::V1)
+            .authenticate(NoiseAuthenticated::xx(&LOCAL_KEY).unwrap())
             .multiplex(mplex::MplexConfig::new())
             .boxed();
 
         // Set the message authenticity - How we expect to publish messages
         // Here we expect the publisher to sign the message with their key.
-        let message_authenticity = MessageAuthenticity::Signed(local_key);
+        let message_authenticity = MessageAuthenticity::Signed(LOCAL_KEY.clone());
 
         // Create a Gossipsub topic
         let topic = IdentTopic::new("test-net");
@@ -41,7 +43,7 @@ impl P2P {
             gossipsub
                 .subscribe(&topic)
                 .expect("could not subscribe to topic");
-            Swarm::new(transport, gossipsub, local_peer_id)
+            Swarm::new(transport, gossipsub, LOCAL_PEER_ID.clone())
         };
 
         // thisd doesnt work: multiaddr not supported
@@ -50,7 +52,7 @@ impl P2P {
         //     .listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
         //     .unwrap();
 
-        let memory: Multiaddr = multiaddr::Protocol::Memory(10).into();
+        let memory = multiaddr::Protocol::Memory(10).into();
         let addr = swarm.listen_on(memory).unwrap();
 
         println!("Listening on {:?}", addr);
